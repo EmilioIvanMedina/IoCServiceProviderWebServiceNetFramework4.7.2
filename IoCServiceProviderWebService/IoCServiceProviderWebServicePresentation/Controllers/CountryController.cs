6b8b25ffafc8b4d862f2ddb985859c8863a27694
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-09-09 17:16:36 -0300

    add a new controller for Country actions

---
+++ IoCServiceProviderWebService/IoCServiceProviderWebServicePresentation/Controllers/CountryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IoCServiceProviderWebServicePresentation.Controllers
+{
+    public class CountryController : ApiController
+    {
+        // GET: api/Country
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ARGENTINA", "PERU" };
+        }
+
+        // GET: api/Country/5
+        public string Get(int id)
+        {
+            return "BOLIVIA";
+        }
+
+        // POST: api/Country
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Country/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Country/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
